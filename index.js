c909a961ba60452254884975970fa1f845c62154
scamgi <scampini.giacomo@gmail.com>
2020-04-11 13:33:41 +0200

    some test code

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 "use strict";
 exports.__esModule = true;
-var fs = require("fs");
-var path = require("path");
-var dirPath = "";
-if (process.argv.length > 0)
-    dirPath = path.join(__dirname, process.argv[0]);
-else
-    dirPath = __dirname;
-console.log("dirpath: " + dirPath);
-console.log("process.argv[0]: " + process.argv[0]);
+// var dirPath = "";
 // process.argv ? dirPath = path.join(__dirname, process.argv[0]) : dirPath = __dirname;
-// var dirPath = path.join(__dirname, process.argv[0]);
-fs.readdir(dirPath, function (err, files) {
-    if (err)
-        console.log('Error: ' + err);
-    for (var _i = 0, files_1 = files; _i < files_1.length; _i++) {
-        var file = files_1[_i];
-        console.log(file);
-    }
-});
+// // var dirPath = path.join(__dirname, process.argv[0]);
+// fs.readdir(dirPath, (err, files) => {
+//   if (err) console.log('Error: ' + err);
+//   for (const file of files) {
+//     console.log(file);
+//   }
+// });
+for (var _i = 0, _a = process.argv; _i < _a.length; _i++) {
+    var item = _a[_i];
+    console.log(item);
+}
