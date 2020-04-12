0ddd5d6c27e82e33ce6eb7cc32836c4838e8645c
scamgi <scampini.giacomo@gmail.com>
2020-04-12 19:10:27 +0200

    colors.js added

--- js/index.js
+++ js/index.js
@@ -3,18 +3,18 @@ exports.__esModule = true;
 var _ = require("lodash");
 var glob = require("glob");
 var variables_1 = require("./variables");
+var colors_1 = require("colors");
 // glob call
 glob(variables_1.pattern, variables_1.options, function (err, files) {
     if (err) {
-        console.log('Error: ' + err.message);
+        console.log(colors_1.red('Error: ' + err.message));
         return;
     }
     _.forEach(files, function (f) {
-        console.log(f);
+        // this checks if f is a directory
+        if (_.endsWith(f, '/')) // if yes
+            console.log(colors_1.green(f));
+        else // if no
+            console.log(f);
     });
 });
-// args = _.slice(args, 2);
-// console.log('Args length is: ' + args.length.toString());
-// _.forEach(args, (item, index) => {
-//   console.log(index.toString() + ": " + item);
-// });
--- ts/index.ts
+++ ts/index.ts
@@ -3,21 +3,20 @@ import * as path from "path";
 import * as _ from "lodash";
 import * as glob from "glob";
 import { options, pattern } from "./variables";
+import { red, bold, green } from "colors";
 
 // glob call
 glob(pattern, options, (err, files) => {
   if (err) {
-    console.log('Error: ' + err.message);
+    console.log(red('Error: ' + err.message));
     return;
   }
 
   _.forEach(files, (f) => {
-    console.log(f);
+    // this checks if f is a directory
+    if (_.endsWith(f, '/')) // if yes
+      console.log(green(f));
+    else // if no
+      console.log(f);
   });
 });
-
-// args = _.slice(args, 2);
-// console.log('Args length is: ' + args.length.toString());
-// _.forEach(args, (item, index) => {
-//   console.log(index.toString() + ": " + item);
-// });
