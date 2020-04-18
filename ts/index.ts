b004883ebc60d965093c214a1e72482554d92b43
scamgi <scampini.giacomo@gmail.com>
2020-04-18 23:57:44 +0200

    display function added

--- ts/index.ts
+++ ts/index.ts
@@ -1,20 +1,14 @@
 import * as _ from "lodash";
 import * as glob from "glob";
 import { glob_options, pattern } from "./variables";
-import { red, bold, green } from "colors";
+import display from "./display";
 
 // glob call
 glob(pattern, glob_options, (err, files) => {
   if (err) {
-    console.log(red('Error: ' + err.message));
+    console.error('Error: ' + err.message);
     return;
   }
 
-  _.forEach(files, (f) => {
-    // this checks if f is a directory
-    if (_.endsWith(f, '/')) // if yes
-      f = green(f);
-
-    console.log(f);
-  });
+  display(files);
 });
