a9eb7681852a0cbf260316e0e60633fbff2c5fdb
scamgi <scampini.giacomo@gmail.com>
2020-04-19 00:39:37 +0200

    closed #4

--- ts/index.ts
+++ ts/index.ts
@@ -3,7 +3,6 @@ import * as glob from "glob";
 import { glob_options, pattern } from "./variables";
 import { red, bold, green } from "colors";
 import { existsSync, lstatSync } from "fs";
-import * as path from "path";
 
 var p = pattern;
 if (existsSync(p) && lstatSync(p).isDirectory()) {
